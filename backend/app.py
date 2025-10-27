a0f62b4ff59577d7499816ea01426dd1a2ff70fa
amarmbbs <amarmbbs00@gmail.com>
2025-10-27 17:15:36 +0530

    Use PORT env variable for deployment

--- backend/app.py
+++ backend/app.py
@@ -394,4 +394,5 @@ if __name__ == '__main__':
         print("   Running in simulation mode only")
         print("   To enable: pip install scapy")
     
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
